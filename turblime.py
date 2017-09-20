332acd65e44fdbc0420041f3ce565ee57d7afedc
Dillon Hafer <dh@dillonhafer.com>
2017-09-20 15:45:26 -0500

    Classify

--- turblime.py
+++ turblime.py
@@ -1,28 +1,31 @@
 import sublime
 import sublime_plugin
 
-def echo_command(working_dir, command):
-  s = "echo 'Running %s' ; bash -cl 'cd %s && %s'" % (command, working_dir, command)
-  return s
+class Turblime(sublime_plugin.WindowCommand):
+  def echo_command(working_dir, command):
+    return "echo 'Running %s' ; bash -cl 'cd %s && %s'" % (command, working_dir, command)
 
-def rspec_command(self, line=False):
-  view = self.window.active_view()
-  project = self.window.folders()[0]
-  path = self.window.active_view().file_name()
+  def rspec_command(self, line=False):
+    view = self.window.active_view()
+    project = self.window.folders()[0]
+    path = self.window.active_view().file_name()
 
-  if line == False:
-    test_command = "rspec %s" % path
-  else:
-    for sel in view.sel():
-      line = view.rowcol(sel.begin())[0] + 1
-    test_command = "rspec %s:%s" % (path, line)
+    if line == False:
+      test_command = "rspec %s" % path
+    else:
+      for sel in view.sel():
+        line = view.rowcol(sel.begin())[0] + 1
+      test_command = "rspec %s:%s" % (path, line)
 
-  return echo_command(project, test_command)
+    return echo_command(project, test_command)
 
-class RspecLineCommand(sublime_plugin.WindowCommand):
+  def command(self, line=False):
+    self.window.active_view().window().run_command('exec', {'shell_cmd': rspec_command(self, line)})
+
+class RspecLineCommand(Turblime):
   def run(self):
-    self.window.active_view().window().run_command('exec', {'shell_cmd': rspec_command(self, True)})
+    self.command(True)
 
-class RspecFileCommand(sublime_plugin.WindowCommand):
+class RspecFileCommand(Turblime):
   def run(self):
-    self.window.active_view().window().run_command('exec', {'shell_cmd': rspec_command(self)})
+    self.command(False)
